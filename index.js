b0f6bf9b0937c68b3e062526a948a0a141d95f63
Corbett <corbett.croslin@gmail.com>
2021-10-18 18:45:40 -0400

    new

---
+++ index.js
@@ -0,0 +1,29 @@
+// var ourArray = [];
+// for (var i = 0; i <= 10; i++) {
+//   ourArray.push(i);
+//   console.log(i);
+// }
+
+
+// var ourArray = [];
+// var i = 0;
+// while(i < 10) {
+//   ourArray.push(i);
+//   i++;
+//   console.log(i);
+// }
+
+
+
+// var ourArray = [];
+// for (var i = 11; i <= 21; i += 2) {
+//   ourArray.push(i);
+// console.log(i);
+// }
+
+
+var ourArray = [];
+for (var i = 15; i > 1; i -= 2) {
+  ourArray.push(i);
+  console.log(i);
+}
